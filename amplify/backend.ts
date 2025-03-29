fbb1fe4d248dd010fed4cce40fc72fd3e1d37583
IndrachapaSenanayaka <senanayakeindrachapa@gmail.com>
2025-03-29 18:44:04 +0530

    edit json

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,11 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
-import { storage } from './storage/resource';
+import { storage, secondaryStorage } from './storage/resource';
 
 defineBackend({
   auth,
   data,
-  storage
+  storage,
+  secondaryStorage
 });
