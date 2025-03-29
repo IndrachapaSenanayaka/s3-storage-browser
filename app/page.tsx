c4485f40d94743b42535a6200f0e920adbeb9599
IndrachapaSenanayaka <senanayakeindrachapa@gmail.com>
2025-03-29 21:15:12 +0530

    add second browser6

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 "use client";
-
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+import outputs from "../config/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import { Authenticator } from '@aws-amplify/ui-react';
 import { StorageBrowser } from '../components/StorageBrowser';
