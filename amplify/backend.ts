44e5ed6db096e2f1f22e53fb2db89cce9f93dbb5
IndrachapaSenanayaka <senanayakeindrachapa@gmail.com>
2025-03-29 20:19:07 +0530

    add second browser4

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,30 +1,15 @@
-import * as s3 from 'aws-cdk-lib/aws-s3';
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
 import { storage, secondaryStorage } from './storage/resource';
 
-// defineBackend({
-//   auth,
-//   data,
-//   storage,
-//   secondaryStorage
-// });
-
-const backend = defineBackend({
+defineBackend({
+  auth,
+  data,
   storage,
   secondaryStorage
 });
 
 
-const s3Bucket = backend.storage.resources.bucket;
-
-
-const cfnBucket = s3Bucket.node.defaultChild as s3.CfnBucket;
-
-
-cfnBucket.accelerateConfiguration = {
-  accelerationStatus: "Enabled" // 'Suspended' if you want to disable transfer acceleration
-}
 
 
